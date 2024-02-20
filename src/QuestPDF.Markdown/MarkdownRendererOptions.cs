33875936778b982927906aa006ab67044c950bce
Christiaan de Ridder <christiaan@channelengine.com>
2024-02-20 09:25:33 +0100

    Use Color struct instead of string values

--- src/QuestPDF.Markdown/MarkdownRendererOptions.cs
+++ src/QuestPDF.Markdown/MarkdownRendererOptions.cs
@@ -1,5 +1,6 @@
 using System.Runtime.InteropServices;
 using QuestPDF.Helpers;
+using QuestPDF.Infrastructure;
 
 namespace QuestPDF.Markdown;
 
@@ -10,28 +11,28 @@ public class MarkdownRendererOptions
     /// </summary>
     public bool Debug { get; set; }
 
-    public string LinkTextColor { get; set; } = Colors.Blue.Medium;
-    public string MarkedTextBackgroundColor { get; set; } = Colors.Yellow.Lighten2;
+    public Color LinkTextColor { get; set; } = Colors.Blue.Medium;
+    public Color MarkedTextBackgroundColor { get; set; } = Colors.Yellow.Lighten2;
 
-    public string BlockQuoteBorderColor { get; set; } = Colors.Grey.Lighten2;
-    public string BlockQuoteTextColor { get; set; } = Colors.Grey.Darken1;
+    public Color BlockQuoteBorderColor { get; set; } = Colors.Grey.Lighten2;
+    public Color BlockQuoteTextColor { get; set; } = Colors.Grey.Darken1;
     public int BlockQuoteBorderThickness { get; set; } = 2;
     
     public string CodeFont { get; set; } = Fonts.CourierNew;
-    public string CodeBlockBackground { get; set; } = Colors.Grey.Lighten4;
-    public string CodeInlineBackground { get; set; } = Colors.Grey.Lighten3;
+    public Color CodeBlockBackground { get; set; } = Colors.Grey.Lighten4;
+    public Color CodeInlineBackground { get; set; } = Colors.Grey.Lighten3;
 
     public string TaskListCheckedGlyph { get; set; } = "☑";
     public string TaskListUncheckedGlyph { get; set; } = "☐";
     public string UnicodeGlyphFont { get; set; } = RuntimeInformation.IsOSPlatform(OSPlatform.OSX) ? "Arial Unicode MS" : Fonts.Arial;
 
-    public string TableBorderColor { get; set; } = Colors.Grey.Lighten2;
-    public string TableEvenRowBackgroundColor { get; set; } = Colors.Grey.Lighten4;
-    public string TableOddRowBackgroundColor { get; set; } = Colors.White;
+    public Color TableBorderColor { get; set; } = Colors.Grey.Lighten2;
+    public Color TableEvenRowBackgroundColor { get; set; } = Colors.Grey.Lighten4;
+    public Color TableOddRowBackgroundColor { get; set; } = Colors.White;
     public int TableHeaderBorderThickness { get; set; } = 3;
     public int TableBorderThickness { get; set; } = 1;
     
-    public string HorizontalRuleColor { get; set; } = Colors.Grey.Lighten2;
+    public Color HorizontalRuleColor { get; set; } = Colors.Grey.Lighten2;
     public int HorizontalRuleThickness { get; set; } = 2;
 
     /// <summary>
