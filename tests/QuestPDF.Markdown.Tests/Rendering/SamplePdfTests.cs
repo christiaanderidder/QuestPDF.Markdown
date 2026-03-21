8799edb2bd88c1ae58d2b2be2b8e4c4f943f42b4
Christiaan de Ridder <christiaan@channelengine.com>
2026-03-21 17:16:59 +0100

    Reformat code

--- tests/QuestPDF.Markdown.Tests/Rendering/SamplePdfTests.cs
+++ tests/QuestPDF.Markdown.Tests/Rendering/SamplePdfTests.cs
@@ -26,7 +26,18 @@ public class SamplePdfTests
         await markdown.DownloadImages();
 
         var document = GenerateDocument(item => item.Markdown(markdown));
-        document.GeneratePdf(Path.Combine(AppContext.BaseDirectory, "..", "..", "..", "..", "tests", "QuestPDF.Markdown.Tests", "test.pdf"));
+        document.GeneratePdf(
+            Path.Combine(
+                AppContext.BaseDirectory,
+                "..",
+                "..",
+                "..",
+                "..",
+                "tests",
+                "QuestPDF.Markdown.Tests",
+                "test.pdf"
+            )
+        );
     }
 
     [Fact(Skip = "This test is not run in CI")]
